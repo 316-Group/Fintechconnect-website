f2251c3d5cd5f2da083d606d13e9e91f8340aa64
LouisKiarie <louiskiarie4@gmail.com>
2026-05-20 14:05:40 +0300

    next config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.NODE_ENV === 'production' ? '/Fintechconnect-website' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Fintechconnect-website/' : '',
+  // This is required for images to work on static exports
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
